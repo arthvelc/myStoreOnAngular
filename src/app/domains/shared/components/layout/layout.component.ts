4caa111485cc023d252481261f76f17ff0dbcb6b
Arturo Solares <asolares.devml@gmail.com>
2024-08-25 11:12:18 -0600

    llegué a las clase 60 , creo que todo va bien

---
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [ CommonModule, HeaderComponent, RouterModule],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent {
+
+}
