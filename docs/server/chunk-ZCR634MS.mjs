c199135ff1994ee02cb24c1cc2cba2f0370ce33a
Arturo Solares <asolares.devml@gmail.com>
2024-10-24 13:47:28 -0600

    chore(config): cambiar el directorio de salida a 'docs'
    - Se modificó la configuración de Angular para cambiar el directorio de salida de  a , con el fin de mejorar la estructura del proyecto o preparar la configuración para despliegue en GitHub Pages.
    
    Archivo modificado:
    - angular.json

---
+++ docs/server/chunk-ZCR634MS.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{q as e}from"./chunk-AMNJ4WKO.mjs";import{J as o,O as i}from"./chunk-VVC5SZPB.mjs";var c=class t{constructor(r){this.http=r}productUrl="https://api.escuelajs.co/api/v1/products";getAllProducts(){return this.http.get(this.productUrl)}getProductById(r){return this.http.get(`${this.productUrl}/${r}`)}static \u0275fac=function(p){return new(p||t)(i(e))};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{c as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-X63LJSSB.mjs";import"./chunk-UCNMCCIX.mjs";import"./chunk-UGF2BP3Y.mjs";import"./chunk-TJNM7AHF.mjs";import"./chunk-AMNJ4WKO.mjs";import"./chunk-VVC5SZPB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
